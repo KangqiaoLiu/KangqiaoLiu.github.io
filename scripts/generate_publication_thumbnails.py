ea6c581c05f043df114c8827d6a77297a71f4084
Kangqiao Liu <75620433+KangqiaoLiu@users.noreply.github.com>
2026-09-18 12:48:06 +0800

    Generate black-bounce thumbnail from v2

--- scripts/generate_publication_thumbnails.py
+++ scripts/generate_publication_thumbnails.py
@@ -24,7 +24,7 @@ SCALE = DPI / 72.0
 FIGURES = {
     "hardwall-ctqw": ("AQ12647.pdf", 8, (318, 40, 575, 190)),
     "qrac": ("qrac_counterexample.pdf", 9, (70, 45, 545, 432)),
-    "gravity-blackbounce": ("2609.18143v1.pdf", 11, (118, 208, 510, 410)),
+    "gravity-blackbounce": ("2609.18143v2.pdf", 12, (118, 208, 510, 410)),
     "gravity-ads": ("2607.00432v2.pdf", 12, (78, 145, 515, 428)),
     "open-frequency": ("2605.03340v1.pdf", 11, (88, 45, 520, 255)),
     "gravity-kn": ("s10052-026-15894-8.pdf", 7, (48, 45, 547, 287)),
